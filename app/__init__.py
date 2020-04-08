bea8003b256bd2e17679d8ebdc4fe422051c7158
YoungkiLyu <youngkiu@vuno.co>
2020-04-08 15:56:35 +0900

    Chapter 13: I18n and L10n

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,9 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
+from flask_babel import lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,9 +20,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 
@@ -50,3 +55,7 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
--- config.py
+++ config.py
@@ -15,3 +15,5 @@ class Config(object):
     ADMINS = ['your-email@example.com']
 
     POSTS_PER_PAGE = 25
+
+    LANGUAGES = ['en', 'es']
